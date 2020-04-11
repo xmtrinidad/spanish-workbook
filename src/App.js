7d3f72583f134345c29f5ae9646753f877142411
xmtrinidad <xmtrinidad@gmail.com>
2020-04-11 14:25:24 -0700

    added app header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import AppHeader from './AppHeader/AppHeader.js';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <h1>Base App</h1>
+      
     </div>
   );
 }
