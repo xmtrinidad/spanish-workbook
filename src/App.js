80b035d2f2bc2de5b18b1c2504f4c9eb9ca84bd7
xmtrinidad <xmtrinidad@gmail.com>
2020-04-20 19:39:04 -0700

    experimenting with markdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 import AppHeader from './AppHeader/AppHeader.js';
 import ExerciseCard from './ExerciseCard/ExerciseCard.js';
 
 function App() {
+  const input = `# Subject Pronouns
+
+  *Function* - To replace a noun that names the subject (the actor) in a clause or sentence
+  
+  | yo            | /                   | nosotros nosotras   | we we                  |
+  |---------------|---------------------|---------------------|------------------------|
+  | tu            | you (informal)      | vosotros vosotras   | you you                |
+  | el ella usted | he she you (formal) | ellos ellas ustedes | they they you (formal) |`;
   return (
     <div className="App">
       <AppHeader />
@@ -13,7 +22,8 @@ function App() {
           <ExerciseCard name="Introduction"></ExerciseCard>
           <ExerciseCard name="Exercise 1-1"></ExerciseCard>
           <ExerciseCard name="Exercise 1-2"></ExerciseCard>
-      </div>
+        </div>
+        <ReactMarkdown source={input} />
       </main>
       
       
---
+++ src/markdown/exercise-1.md
@@ -0,0 +1,8 @@
+# Subject Pronouns
+
+```Function``` - To replace a noun that names the subject (the actor) in a clause or sentence
+
+| yo            | /                   | nosotros nosotras   | we we                  |
+|---------------|---------------------|---------------------|------------------------|
+| tu            | you (informal)      | vosotros vosotras   | you you                |
+| el ella usted | he she you (formal) | ellos ellas ustedes | they they you (formal) |
